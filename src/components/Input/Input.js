3a4d5171f667cc9a12e61b0f8edc398046882cac
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-10-08 14:02:46 +0200

    Adding routing and component conditional rendering

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,36 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Input.module.scss';
 
-const Input = ({ tag: Tag, name, maxLength, label }) => {
-  return (
-    <div className={styles.formItem}>
-      <Tag
-        className={Tag === 'textarea' ? styles.textarea : styles.input}
-        type='text'
-        name={name}
-        id={name}
-        required
-        maxLength={maxLength}
-        placeholder=' '
-      />
-      <label className={styles.label} htmlFor={label}>
-        {label}
-      </label>
-      <div className={styles.formItemBar} />
-    </div>
-  );
-};
+const Input = ({tag: Tag, name, label, maxLength}) => (
+  <div className={styles.formItem}>
+    <Tag
+      className={Tag === 'textarea' ? styles.textarea : styles.input}
+      type="text"
+      name={name}
+      id={name}
+      required
+      maxLength={maxLength}
+      placeholder=" "
+    />
+    <label className={styles.label} htmlFor={name}>
+      {label}
+    </label>
+    <div className={styles.formItemBar} />
+  </div>
+);
 
 Input.propTypes = {
   tag: PropTypes.string,
   name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
   maxLength: PropTypes.number,
-  label: PropTypes.string.isRequired
-};
+}
 
 Input.defaultProps = {
   tag: 'input',
-  maxLength: 200
-};
+  maxLength: 200,
+}
 
 export default Input;
